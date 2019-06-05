b5fe452fd10b2474baed3d5dd771c6cfe14a4fd1
zodiacxs <zodiacxs@gmail.com>
2019-06-05 11:06:50 +1000

    LOL

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Optus from './Optus';
 
-//import Optus2 from './Optus2';
-// ReactDOM.render(<Optus2 />, document.getElementById('root'));
-
  ReactDOM.render(<Optus />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
