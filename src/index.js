81940fc5dbb440280efedb9e4094b6a9021bb8f9
zodiacxs <zodiacxs@gmail.com>
2019-06-05 11:24:24 +1000

    minor change

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Optus from './Optus';
 
- ReactDOM.render(<Optus />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Optus />, document.getElementById('root'));
